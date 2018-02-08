0876f34c8462336b7d855f1f4d14712a3b2936cc
LauraMihalca <lauramihalca19@gmail.com>
2018-02-08 20:50:16 +0200

    Change Pass

--- src/main/java/com/shaAlgorithmDemo/spring/security/service/UserService.java
+++ src/main/java/com/shaAlgorithmDemo/spring/security/service/UserService.java
@@ -2,6 +2,7 @@ package com.shaAlgorithmDemo.spring.security.service;
 
 import com.shaAlgorithmDemo.spring.security.model.User;
 import com.shaAlgorithmDemo.spring.security.web.dto.UserRegistrationDto;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
@@ -11,4 +12,10 @@ public interface UserService extends UserDetailsService {
     User findByUsername(String username);
 
     User save(UserRegistrationDto registration);
+
+    User save(User user);
+
+    UserDetails currentUserDetails();
+
+    String encodeString(String s);
 }
